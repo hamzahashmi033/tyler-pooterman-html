011de75457524b7853516ecc5c4b71d4aaab432d
Syed Hamza Hashmi <92597895+hamzahashmi033@users.noreply.github.com>
2026-04-18 06:50:59 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import $ from "jquery";
 import App from "./App.jsx";
+import $ from "jquery";
 
 window.$ = $;
 window.jQuery = $;
 
-// JS
-import "./js/bootstrap.min.js";
-import "./js/swiper-bundle.min.js";
-import "./js/carousel.js";
-import "./js/jquery.nice-select.min.js";
-import "./js/animation_heading.js";
-import "./js/rangle-slider.js";
-import "./js/shortcodes.js";
-import "./js/lazysize.min.js";
+// Load plugins AFTER jQuery is set
+await import("./js/bootstrap.min.js");
+await import("./js/swiper-bundle.min.js");
+await import("./js/carousel.js");
+await import("./js/jquery.nice-select.min.js");
+await import("./js/animation_heading.js");
+await import("./js/rangle-slider.js");
+await import("./js/shortcodes.js");
+await import("./js/lazysize.min.js");
 
 // CSS
 import "./fonts/fonts.css";
@@ -24,6 +23,4 @@ import "./css/swiper-bundle.min.css";
 import "./css/animate.css";
 import "./css/styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <App />
-);
+createRoot(document.getElementById("root")).render(<App />);
