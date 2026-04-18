0ba96d1b541184e3311d9f3ff2c6a3dfd40be38b
Syed Hamza Hashmi <92597895+hamzahashmi033@users.noreply.github.com>
2026-04-18 06:49:55 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import $ from "jquery";
+import App from "./App.jsx";
 
-// Import jQuery and set globals before other scripts
+window.$ = $;
+window.jQuery = $;
 
-import './js/jquery.min.js';
-window.jQuery = window.$ = window.jQuery || window.$;
-import './js/bootstrap.min.js'
-import './js/swiper-bundle.min.js'
-import './js/carousel.js'
-// import './js/plugin.js'
-import './js/jquery.nice-select.min.js'
-import './js/animation_heading.js'
-import './js/rangle-slider.js'
-import './js/shortcodes.js'
-import './js/lazysize.min.js'
-import './js/main.js'
+// JS
+import "./js/bootstrap.min.js";
+import "./js/swiper-bundle.min.js";
+import "./js/carousel.js";
+import "./js/jquery.nice-select.min.js";
+import "./js/animation_heading.js";
+import "./js/rangle-slider.js";
+import "./js/shortcodes.js";
+import "./js/lazysize.min.js";
 
-import './fonts/fonts.css';
-import './fonts/font-icons.css';
-import './css/bootstrap.min.css';
-import './css/swiper-bundle.min.css';
-import './css/animate.css';
-import './css/styles.css';
+// CSS
+import "./fonts/fonts.css";
+import "./fonts/font-icons.css";
+import "./css/bootstrap.min.css";
+import "./css/swiper-bundle.min.css";
+import "./css/animate.css";
+import "./css/styles.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
+createRoot(document.getElementById("root")).render(
+  <App />
+);
